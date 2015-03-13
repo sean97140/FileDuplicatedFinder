68ffb2b21d2368c14871309f737b5a9a73235f57
Sean Mahan <mahan2@pdx.edu>
2015-03-13 16:48:34 -0700

    all files added

---
+++ WindowsFormsApplication2/BST.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//code modified from https://msdn.microsoft.com/en-us/library/ms379572%28v=vs.80%29.aspx
+
+namespace RecursiveSearchCS
+{
+    public class HashInfo 
+    {
+        public string hash;
+        public string filename;
+        public override string ToString() {
+            return filename;
+        }
+        public string GetHashString()
+        {
+            return hash;
+        }
+    }
+    
+    
+    public class BinaryTree
+    {
+        private BinaryTreeNode root;
+        public ArrayList duplicates = new ArrayList();
+        public double totalDuplicateSize;
+        public int duplicateCount = 0;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public virtual void Clear()
+        {
+            root = null;
+        }
+
+        public BinaryTreeNode Root
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                root = value;
+            }
+        }
+
+    
+        public virtual void Add(HashInfo data)
+        {
+            BinaryTreeNode n = new BinaryTreeNode(data);
+            int result;
+            BinaryTreeNode current = root, parent = null;
+
+            while (current != null)
+            {
+                result = String.Compare(current.Value.GetHashString(), data.GetHashString());
+                if (result == 0)
+                {
+                    duplicateCount++;
+                    duplicates.Add(current.Value.ToString() + " copy of: " + data.ToString());
+                    totalDuplicateSize += new FileInfo(data.ToString()).Length / 1024.0;
+                    return;
+                }
+                else if (result > 0)
+                {
+
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (result < 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }
+            }
+
+            if (parent == null)
+                root = n;
+            else
+            {
+                result = String.Compare(parent.Value.GetHashString(), data.GetHashString());
+                if (result > 0)
+                    parent.Left = n;
+                else
+                    parent.Right = n;
+            }
+        }
+    }
+    
+    public class BinaryTreeNode : Node
+    {
+        public BinaryTreeNode(HashInfo data) : base(data, null) { }
+
+        public BinaryTreeNode Left
+        {
+            get
+            {
+                if (base.Neighbors == null)
+                    return null;
+                else
+                    return (BinaryTreeNode)base.Neighbors[0];
+            }
+            set
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new NodeList(2);
+
+                base.Neighbors[0] = value;
+            }
+        }
+
+        public BinaryTreeNode Right
+        {
+            get
+            {
+                if (base.Neighbors == null)
+                    return null;
+                else
+                    return (BinaryTreeNode)base.Neighbors[1];
+            }
+            set
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new NodeList(2);
+
+                base.Neighbors[1] = value;
+            }
+        }
+    }
+    
+    public class NodeList : Collection<Node>
+{
+ 
+    public NodeList(int initialSize)
+    {
+        // Add the specified number of items
+        for (int i = 0; i < initialSize; i++)
+            base.Items.Add(default(Node));
+    }
+}
+    
+    public class Node
+{
+        // Private member-variables
+        private HashInfo data;
+        private NodeList neighbors = null;
+
+        public Node() {}
+        public Node(HashInfo data) : this(data, null) {}
+        public Node(HashInfo data, NodeList neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+
+        public HashInfo Value
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+
+        protected NodeList Neighbors
+        {
+            get
+            {
+                return neighbors;
+            }
+            set
+            {
+                neighbors = value;
+            }
+        }
+    }
+}
+
---
+++ WindowsFormsApplication2/Program.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Security.Cryptography;
+
+
+//Modified example code from: http://support.microsoft.com/en-us/kb/303974
+namespace RecursiveSearchCS
+{
+    /// <summary>
+    /// Summary description for Form1
+    /// </summary>
+    public class DuplicationFinderForm : System.Windows.Forms.Form
+    {
+        internal System.Windows.Forms.Button btnSearch;
+        internal System.Windows.Forms.Label lblDirectory;
+        internal System.Windows.Forms.ListBox lstFilesFound;
+        internal System.Windows.Forms.ComboBox cboDirectory;
+        /// <summary>
+        /// Required designer variable
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+        private Button ProcessBtn;
+        private ProgressBar progressBar1;
+        ArrayList fileList = new ArrayList();
+        private Label processingLabel;
+        private Label sizeLabel;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button Browse;
+        long totalSize;
+
+
+        public DuplicationFinderForm()
+        {
+            // 
+            // Required for Windows Form Designer support
+            // 
+            InitializeComponent();
+
+            // 
+            // TODO: Add any constructor code after InitializeComponent call.
+            // 
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support: do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.lstFilesFound = new System.Windows.Forms.ListBox();
+            this.cboDirectory = new System.Windows.Forms.ComboBox();
+            this.ProcessBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.processingLabel = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Browse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 268);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.Location = new System.Drawing.Point(13, 244);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(55, 23);
+            this.lblDirectory.TabIndex = 3;
+            this.lblDirectory.Text = "Look In:";
+            // 
+            // lstFilesFound
+            // 
+            this.lstFilesFound.HorizontalScrollbar = true;
+            this.lstFilesFound.Location = new System.Drawing.Point(12, 12);
+            this.lstFilesFound.Name = "lstFilesFound";
+            this.lstFilesFound.Size = new System.Drawing.Size(679, 225);
+            this.lstFilesFound.TabIndex = 1;
+            // 
+            // cboDirectory
+            // 
+            this.cboDirectory.DropDownWidth = 112;
+            this.cboDirectory.Location = new System.Drawing.Point(74, 243);
+            this.cboDirectory.Name = "cboDirectory";
+            this.cboDirectory.Size = new System.Drawing.Size(536, 21);
+            this.cboDirectory.TabIndex = 2;
+            this.cboDirectory.Text = "ComboBox1";
+            // 
+            // ProcessBtn
+            // 
+            this.ProcessBtn.Enabled = false;
+            this.ProcessBtn.Location = new System.Drawing.Point(93, 268);
+            this.ProcessBtn.Name = "ProcessBtn";
+            this.ProcessBtn.Size = new System.Drawing.Size(75, 23);
+            this.ProcessBtn.TabIndex = 6;
+            this.ProcessBtn.Text = "Process";
+            this.ProcessBtn.UseVisualStyleBackColor = true;
+            this.ProcessBtn.Click += new System.EventHandler(this.ProcessBtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(174, 268);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(517, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // processingLabel
+            // 
+            this.processingLabel.AutoSize = true;
+            this.processingLabel.Location = new System.Drawing.Point(12, 298);
+            this.processingLabel.Name = "processingLabel";
+            this.processingLabel.Size = new System.Drawing.Size(64, 13);
+            this.processingLabel.TabIndex = 8;
+            this.processingLabel.Text = "Not working";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(12, 311);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(33, 13);
+            this.sizeLabel.TabIndex = 9;
+            this.sizeLabel.Text = "Size: ";
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(616, 241);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 10;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(704, 332);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.processingLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.ProcessBtn);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.lstFilesFound);
+            this.Controls.Add(this.cboDirectory);
+            this.Name = "Form1";
+            this.Text = "Duplicate File Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new DuplicationFinderForm());
+        }
+
+        private void btnSearch_Click(object sender, System.EventArgs e)
+        {
+            lstFilesFound.Items.Clear();
+            cboDirectory.Enabled = false;
+            btnSearch.Text = "Searching...";
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
+            DirSearch(cboDirectory.Text, false);
+            btnSearch.Text = "Search";
+            this.Cursor = Cursors.Default;
+            btnSearch.Enabled = false;
+            ProcessBtn.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            cboDirectory.Items.Clear();
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                cboDirectory.Items.Add(s);
+            }
+            cboDirectory.Text = "C:\\";
+
+        }
+        void searchFiles(string sDir)
+        {
+            foreach (string f in Directory.GetFiles(sDir, "*.*"))
+            {
+                try
+                {
+                    fileList.Add(f);
+                }
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                }
+
+            }
+
+            DirectoryInfo di = new DirectoryInfo(sDir);
+            FileInfo[] fiArr = di.GetFiles();
+
+            foreach (FileInfo f in fiArr)
+                totalSize += f.Length / 8192;
+
+        }
+        void DirSearch(string sDir, bool baseSearched)
+        {
+
+            if (!baseSearched)
+            {
+                searchFiles(sDir);
+            }
+
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                try
+                {
+                    searchFiles(d);
+                    DirSearch(d, true);
+                }
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                }
+
+            }
+        }
+
+        private void ProcessBtn_Click(object sender, EventArgs e)
+        {
+            ProcessBtn.Text = "Processing...";
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
+            progressBar1.Maximum = (int)totalSize;
+            BinaryTree bst = new BinaryTree();
+
+            foreach (string filename in fileList)
+            {
+                // modified code from: http://stackoverflow.com/questions/10520048/calculate-md5-checksum-for-a-file
+                using (var md5 = MD5.Create())
+                {
+                    try
+                    {
+                        using (var stream = File.OpenRead(filename))
+                        {
+                            HashInfo temp = new HashInfo();
+                            processingLabel.Text = "Currently processing file: " + filename;
+                            sizeLabel.Text = "Size: " + Math.Round(new FileInfo(filename).Length / 1048576.0, 2).ToString() + " MB  Total size of duplicates found: " + Math.Round(bst.totalDuplicateSize / 1024, 2) + " MB  Duplicates Found: " + bst.duplicateCount;
+                            Application.DoEvents();
+                            temp.hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                            temp.filename = filename;
+                            bst.Add(temp);
+                            progressBar1.Increment((int)new FileInfo(filename).Length / 8192);
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        //ignore file
+                    }
+                }
+            }
+            foreach (string dupliacte in bst.duplicates)
+            {
+                lstFilesFound.Items.Add(dupliacte);
+            }
+
+            ProcessBtn.Text = "Process";
+            this.Cursor = Cursors.Default;
+            cboDirectory.Enabled = true;
+            ProcessBtn.Enabled = false;
+            btnSearch.Enabled = true;
+            fileList.Clear();
+            bst.Clear();
+            progressBar1.Value = 0;
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            cboDirectory.Items.Clear();
+            cboDirectory.Items.Add(folderBrowserDialog1.SelectedPath);
+            cboDirectory.Text = folderBrowserDialog1.SelectedPath;
+
+        }
+
+    }
+}
